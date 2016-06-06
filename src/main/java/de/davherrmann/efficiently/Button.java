4d57b297f6c2d0b99285eec9f53596b9e99b2685
David Herrmann <davherrmann@googlemail.com>
2016-06-06 08:47:16 +0200

    implement button onClick

--- src/main/java/de/davherrmann/efficiently/Button.java
+++ src/main/java/de/davherrmann/efficiently/Button.java
@@ -1,15 +1,19 @@
 package de.davherrmann.efficiently;
 
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Component;
 
+import de.davherrmann.efficiently.server.Action;
 import de.davherrmann.efficiently.server.Dispatcher;
 
-public class Button implements Element
+public class Button implements Element, Bindable
 {
     private final com.vaadin.ui.Button button = new com.vaadin.ui.Button();
+    private final Dispatcher dispatcher;
+
     public Button(final Dispatcher dispatcher)
     {
-
+        this.dispatcher = dispatcher;
     }
 
     @Override
@@ -22,5 +26,15 @@ public class Button implements Element
     public Component component()
     {
         return button;
+    }
+
+    @Override
+    public void changeProperty(final String property, final Object value)
+    {
+        if ("onClick".equals(property))
+        {
+            button.removeListener(ClickEvent.class, button);
+            button.addClickListener(e -> dispatcher.dispatch(new Action((String) value)));
+        }
     }
 }
